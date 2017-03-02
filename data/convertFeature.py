866350fe6db8d649ce42f20eec968ae968dc92c2
zhy9036 <zhangyang9036@gmail.com>
2017-03-01 16:42:06 -0800

    hourly & eculidean_dis

--- data/convertFeature.py
+++ data/convertFeature.py
@@ -1,4 +1,4 @@
-import csv, os
+import csv, os, numpy
 
 def convertToHour(stepList):
 	hourlist = []
@@ -20,18 +20,31 @@ def convertToHour(stepList):
 			rst.append(1) if hourlist[i] >= day_avg else rst.append(0)
 	return rst
 
+def euclidean_dis(a, b):
+	a = numpy.array(map(int, a))
+	b = numpy.array(map(int, b))
+	return numpy.linalg.norm(a-b)
 
 def convert(filename):
 	new_rows = []
 	with open (filename, 'rb') as f:
 		reader = csv.reader(f)
+		count = 0
 		for row in reader:
 			hourlist = convertToHour(row)
+			if count > 0:
+				pre = new_rows[count-1]
+				dis = euclidean_dis(hourlist[1:], pre[1: len(pre)-1])
+				hourlist.append(dis)
+			else:
+				hourlist.append('N/A')
 			new_rows.append(hourlist)
+			count = count + 1
 	with open ('../hourly/' + 'hour_' + filename, 'wb') as f:
 		writer = csv.writer(f)
 		writer.writerows(new_rows)
 			
+
 
 dir = '/home/leozhang/Desktop/fitbit/data'
 if not os.path.exists('../hourly'):
